d4fdfcff2df8b3691f29fc54b560df4944cef036
SUXUING-star <jieyalinjiu@gmail.com>
2024-12-29 14:21:47 +0800

    up

--- next,config.js
+++ next,config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+      appDir: true
+    },
     async rewrites() {
       return [
         {
           source: '/api/:path*',
-          destination: '/api/:path*',
+          destination: '/api/:path*'
         }
       ]
     }
